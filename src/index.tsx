3f14e1950710fa6f20946a79096ea416460dfd8b
Vladislav Maraev <vlad@maraev.me>
2021-12-11 18:15:11 +0100

    Add support for pagination
    This change allows the frontend to read available book segments (DDDs)
    and updates static html page with the buttons for to the available
    DDDs. Clicking a button sets a haptic input which is sent immediately
    after start_session request to TDM.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from "react-dom";
 import { Machine, assign, actions, State } from "xstate";
 import { useMachine, asEffect } from "@xstate/react";
 import { tdmDmMachine } from "./tdmClient";
+import { inspect } from "@xstate/inspect";
 
 import createSpeechRecognitionPonyfill from 'web-speech-cognitive-services/lib/SpeechServices/SpeechToText'
 import createSpeechSynthesisPonyfill from 'web-speech-cognitive-services/lib/SpeechServices/TextToSpeech';
@@ -19,14 +20,25 @@ const REGION = 'northeurope';
 
 const defaultPassivity = 5
 
+
+if (process.env.NODE_ENV === 'development') {
+    inspect({
+        url: "https://statecharts.io/inspect",
+        iframe: false
+    });
+}
+
+
 const machine = Machine<SDSContext, any, SDSEvent>({
     id: 'root',
     type: 'parallel',
+    invoke: {
+        src: 'checkForPage',
+    },
     states: {
         dm: {
             ...dm
         },
-
         gui: {
             initial: 'micOnly',
             states: {
@@ -35,162 +47,158 @@ const machine = Machine<SDSContext, any, SDSEvent>({
                 },
                 showAlternatives: {
                     on: { SELECT: 'micOnly' },
-                }
-            }
+                },
+            },
         },
-
         asrtts: {
-            initial: 'init',
+            initial: 'initialize',
             states: {
-                init: {
+                initialize: {
+                    initial: 'await',
                     on: {
-                        CLICK: {
-                            target: 'getToken',
-                            actions: [
-                                assign({
-                                    audioCtx: (_ctx) =>
-                                        new ((window as any).AudioContext || (window as any).webkitAudioContext)()
-                                }),
-                                (context) =>
-                                    navigator.mediaDevices.getUserMedia({ audio: true })
-                                        .then(function(stream) { context.audioCtx.createMediaStreamSource(stream) })
-                            ]
-                        }
-                    }
-                },
-                getToken: {
-                    invoke: {
-                        id: "getAuthorizationToken",
-                        src: (_ctx, _evt) => getAuthorizationToken(),
-                        onDone: {
-                            actions: [
-                                assign((_context, event) => { return { azureAuthorizationToken: event.data } }),
-                                'ponyfillASR'],
-                            target: 'ponyfillTTS'
-                        },
-                        onError: {
-                            target: 'fail'
-                        }
-                    }
-                },
-                ponyfillTTS: {
-                    invoke: {
-                        id: 'ponyTTS',
-                        src: (context, _event) => (callback, _onReceive) => {
-                            const ponyfill = createSpeechSynthesisPonyfill({
-                                audioContext: context.audioCtx,
-                                credentials: {
-                                    region: REGION,
-                                    authorizationToken: context.azureAuthorizationToken,
-                                }
-                            });
-                            const { speechSynthesis, SpeechSynthesisUtterance } = ponyfill;
-                            context.tts = speechSynthesis
-                            context.ttsUtterance = SpeechSynthesisUtterance
-                            context.tts.addEventListener('voiceschanged', () => {
-                                context.tts.cancel()
-                                const voices = context.tts.getVoices();
-                                let voiceRe = RegExp("en-US", 'u')
-                                if (Config.TTS_VOICE) {
-                                    voiceRe = RegExp(Config.TTS_VOICE, 'u')
-                                }
-                                const voice = voices.find((v: any) => voiceRe.test(v.name))!
-                                if (voice) {
-                                    context.voice = voice
-                                    callback('TTS_READY')
-                                } else {
-                                    console.error(`TTS_ERROR: Could not get voice for regexp ${voiceRe}`)
-                                    callback('TTS_ERROR')
-                                }
-                            })
-                        }
-                    },
-                    on: {
-                        TTS_READY: 'idle',
-                        TTS_ERROR: 'fail'
-                    }
-                },
-                idle: {
-                    on: {
-                        LISTEN: 'recognising',
-                        SPEAK: {
-                            target: 'speaking',
-                            actions: assign((_context, event) => { return { ttsAgenda: event.value } })
-                        }
-                    },
-                },
-                recognising: {
-                    initial: 'noinput',
-                    exit: 'recStop',
-                    on: {
-                        ASRRESULT: {
-                            actions: ['recLogResult',
-                                assign((_context, event) => {
-                                    return {
-                                        recResult: event.value
-                                    }
-                                })],
-                            target: '.match'
-                        },
-                        RECOGNISED: 'idle',
-                        SELECT: 'idle',
-                        CLICK: '.pause',
-                        TIMEOUT: '#root.asrtts.idle'
+                        TTS_READY: 'ready',
+                        TTS_ERROR: '.fail'
                     },
                     states: {
-                        noinput: {
-                            entry: [
-                                'recStart',
-                                send(
-                                    { type: 'TIMEOUT' },
-                                    {
-                                        delay: (context) => (1000 * (context.tdmPassivity ?? defaultPassivity)),
-                                        id: 'timeout'
-                                    }
-                                )],
+                        fail: {},
+                        await: {
                             on: {
-                                STARTSPEECH: 'inprogress'
+                                CLICK: {
+                                    target: 'getToken',
+                                    actions: [
+                                        assign({
+                                            audioCtx: (_ctx) =>
+                                                new ((window as any).AudioContext || (window as any).webkitAudioContext)()
+                                        }),
+                                        (context) =>
+                                            navigator.mediaDevices.getUserMedia({ audio: true })
+                                                .then(function(stream) { context.audioCtx.createMediaStreamSource(stream) })
+                                    ]
+                                }
+                            }
+                        },
+                        getToken: {
+                            invoke: {
+                                id: "getAuthorizationToken",
+                                src: (_ctx, _evt) => getAuthorizationToken(),
+                                onDone: {
+                                    actions: [
+                                        assign((_context, event) => { return { azureAuthorizationToken: event.data } }),
+                                        'ponyfillASR'],
+                                    target: 'ponyfillTTS'
+                                },
+                                onError: {
+                                    target: 'fail'
+                                }
+                            }
+                        },
+                        ponyfillTTS: {
+                            invoke: {
+                                id: 'ponyTTS',
+                                src: (context, _event) => (callback, _onReceive) => {
+                                    const ponyfill = createSpeechSynthesisPonyfill({
+                                        audioContext: context.audioCtx,
+                                        credentials: {
+                                            region: REGION,
+                                            authorizationToken: context.azureAuthorizationToken,
+                                        }
+                                    });
+                                    const { speechSynthesis, SpeechSynthesisUtterance } = ponyfill;
+                                    context.tts = speechSynthesis
+                                    context.ttsUtterance = SpeechSynthesisUtterance
+                                    context.tts.addEventListener('voiceschanged', () => {
+                                        context.tts.cancel()
+                                        const voices = context.tts.getVoices();
+                                        let voiceRe = RegExp("en-US", 'u')
+                                        if (Config.TTS_VOICE) {
+                                            voiceRe = RegExp(Config.TTS_VOICE, 'u')
+                                        }
+                                        const voice = voices.find((v: any) => voiceRe.test(v.name))!
+                                        if (voice) {
+                                            context.voice = voice
+                                            callback('TTS_READY')
+                                        } else {
+                                            console.error(`TTS_ERROR: Could not get voice for regexp ${voiceRe}`)
+                                            callback('TTS_ERROR')
+                                        }
+                                    })
+                                }
                             },
-                            exit: cancel('timeout')
-                        },
-                        inprogress: {
-                        },
-                        match: {
-                            entry: send('RECOGNISED'),
-                        },
-                        pause: {
-                            entry: 'recStop',
-                            on: { CLICK: 'noinput' }
                         }
-                    }
-                },
-                speaking: {
-                    entry: ['recStop', 'ttsStart'],
-                    on: {
-                        ENDSPEECH: 'idle',
-                        SELECT: 'idle',
-                        CLICK: { target: 'idle', actions: send('ENDSPEECH') }
                     },
-                    exit: 'ttsStop',
                 },
-                fail: {}
-            }
-        }
-    },
-},
-    {
-        actions: {
-            recLogResult: (context: SDSContext) => {
-                /* context.recResult = event.recResult; */
-                console.log('U>', context.recResult[0]["utterance"],
-                    { "confidence": context.recResult[0]["confidence"] });
+                ready: {
+                    initial: 'idle',
+                    states: {
+                        idle: {
+                            on: {
+                                LISTEN: 'recognising',
+                                SPEAK: {
+                                    target: 'speaking',
+                                    actions: assign((_context, event) => { return { ttsAgenda: event.value } })
+                                }
+                            },
+                        },
+                        recognising: {
+                            initial: 'noinput',
+                            exit: 'recStop',
+                            on: {
+                                ASRRESULT: {
+                                    actions: ['recLogResult',
+                                        assign((_context, event) => {
+                                            return {
+                                                recResult: event.value
+                                            }
+                                        })],
+                                    target: '.match'
+                                },
+                                RECOGNISED: 'idle',
+                                SELECT: 'idle',
+                                CLICK: '.pause',
+                                TIMEOUT: '#root.asrtts.ready.idle'
+                            },
+                            states: {
+                                noinput: {
+                                    entry: [
+                                        'recStart',
+                                        send(
+                                            { type: 'TIMEOUT' },
+                                            {
+                                                delay: (context) => (1000 * (context.tdmPassivity ?? defaultPassivity)),
+                                                id: 'timeout'
+                                            }
+                                        )],
+                                    on: {
+                                        STARTSPEECH: 'inprogress'
+                                    },
+                                    exit: cancel('timeout')
+                                },
+                                inprogress: {
+                                },
+                                match: {
+                                    entry: send('RECOGNISED'),
+                                },
+                                pause: {
+                                    entry: 'recStop',
+                                    on: { CLICK: 'noinput' }
+                                }
+                            }
+                        },
+                        speaking: {
+                            entry: ['recStop', 'ttsStart'],
+                            on: {
+                                ENDSPEECH: 'idle',
+                                SELECT: 'idle',
+                                CLICK: { target: 'idle', actions: send('ENDSPEECH') }
+                            },
+                            exit: 'ttsStop',
+                        },
+                    },
+                },
             },
-            logIntent: (context: SDSContext) => {
-                /* context.nluData = event.data */
-                console.log('<< NLU intent: ' + context.nluData.intent.name)
-            }
         },
-    });
+    },
+});
 
 
 
@@ -207,14 +215,14 @@ const ReactiveButton = (props: Props): JSX.Element => {
     switch (true) {
         case props.state.matches({ asrtts: 'fail' }) || props.state.matches({ dm: 'fail' }):
             break;
-        case props.state.matches({ asrtts: { recognising: 'pause' } }):
+        case props.state.matches({ asrtts: { ready: { recognising: 'pause' } } }):
             promptText = "Click to continue"
             break;
-        case props.state.matches({ asrtts: 'recognising' }):
+        case props.state.matches({ asrtts: { ready: 'recognising' } }):
             circleClass = "circle-recognising"
             promptText = promptText || 'Listening...'
             break;
-        case props.state.matches({ asrtts: 'speaking' }):
+        case props.state.matches({ asrtts: { ready: 'speaking' } }):
             circleClass = "circle-speaking"
             promptText = promptText || 'Speaking...'
             break;
@@ -260,9 +268,32 @@ const FigureButton = (props: Props): JSX.Element => {
     )
 }
 
+const tdmContext = { segment: { pageNumber: 0, dddName: "cover" } }
 function App() {
-    const [current, send] = useMachine(machine, {
+    const [current, send] = useMachine(machine.withContext({ ...machine.context, ...tdmContext }), {
+        devTools: process.env.NODE_ENV === 'development' ? true : false,
+        services: {
+            checkForPage: () => (send) => {
+                const listener = (e: any) => {
+                    send({ type: 'TURNPAGE', value: e.detail });
+                };
+
+                window.addEventListener('turnpage', listener);
+
+                return () => {
+                    window.removeEventListener('turnpage', listener);
+                };
+            },
+        },
         actions: {
+            setAvailableDDDs: asEffect((context) => {
+                const event = new CustomEvent<any>('setAvailableDDDs', { detail: context.tdmAvailableDDDs });
+                window.dispatchEvent(event);
+            }),
+            recLogResult: (context: SDSContext) => {
+                console.log('U>', context.recResult[0]["utterance"],
+                    { "confidence": context.recResult[0]["confidence"] });
+            },
             recStart: asEffect((context) => {
                 context.asr.start()
                 /* console.log('Ready to receive a voice input.'); */
