bac564d1e4f237c76f552a4dfd06c0889d05cec9
Fredrik Garneij <fredrik.garneij@gmail.com>
2022-09-06 17:31:29 +0200

    Update start-nginx.sh
    Add cors header to nginx config file

--- start-nginx.sh
+++ start-nginx.sh
@@ -5,4 +5,5 @@ for file in /tmp/*.js;
 do
   cat $file | envsubst $EXISTING_VARS | tee $JSFOLDER/$(basename $file)
 done
+sed -i '/^   }/i add_header Access-Control-Allow-Origin *;' /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
